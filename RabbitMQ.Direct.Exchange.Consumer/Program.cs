6f5ca7290c7233b496a27099298af3919d985321
isatalhamengi <italhamengi@gmail.com>
2024-02-15 09:21:35 +0300

    DiretExhange Example

---
+++ RabbitMQ.Direct.Exchange.Consumer/Program.cs
@@ -0,0 +1,37 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+ConnectionFactory connectionFactory = new();
+connectionFactory.Uri = new("your_url");
+
+using IConnection connection = connectionFactory.CreateConnection();
+using IModel channel = connection.CreateModel();
+
+//1. Adım
+channel.ExchangeDeclare(exchange: "direct-exchange-example", type: ExchangeType.Direct);
+
+//2. Adım
+string queueName = channel.QueueDeclare();
+
+//3. Adım
+channel.QueueBind(queue: queueName, exchange: "direct-exchange-example", routingKey: "direct-queue-example");
+
+EventingBasicConsumer consumer = new EventingBasicConsumer(channel);
+
+channel.BasicConsume(queue: queueName, autoAck: true, consumer: consumer);
+
+consumer.Received += (sender, e) =>
+{
+    string message = Encoding.UTF8.GetString(e.Body.Span);
+    Console.WriteLine(message);
+};
+
+Console.Read();
+
+
+//1. Adım : Publisher'da ki exhange ile birebir aynı isim ve type'a sahip bir exchange tanımlanır
+
+//2. Adım : Pusblisher Tarafından routing key'de bulunan değerdeki kuyruğa gönderilen mesajları kendi oluşturduğumuz kuyruğa yönlendirerek tüketmemiz gerekmektedir. Bunun için önce bir kuyruk oluşturulmalıdır.
+
+//3. Adım : 
---
+++ RabbitMQ.Direct.Exchange.Publisher/Program.cs
@@ -0,0 +1,21 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+ConnectionFactory connectionFactory = new();
+connectionFactory.Uri = new("your_url");
+
+using IConnection connection = connectionFactory.CreateConnection();
+using IModel channel = connection.CreateModel();
+
+channel.ExchangeDeclare(exchange: "direct-exchange-example", type: ExchangeType.Direct);
+
+while (true)
+{
+    Console.Write("Mesaj : ");
+    string message = Console.ReadLine();
+    byte[] byteMessage = Encoding.UTF8.GetBytes(message);
+
+    channel.BasicPublish(exchange: "direct-exchange-example", routingKey: "direct-queue-example", body: byteMessage);
+}
+
+Console.Read();
