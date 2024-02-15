9168fbefbfbce65506b8b2715e475e9fb7afd5b0
isatalhamengi <italhamengi@gmail.com>
2024-02-15 09:57:44 +0300

    Fannout Exhange Example

---
+++ RabbitMQ.Fanout.Exchange.Consumer/Program.cs
@@ -0,0 +1,30 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+ConnectionFactory connectionFactory = new();
+connectionFactory.Uri = new("your_url");
+
+using IConnection connection = connectionFactory.CreateConnection();
+using IModel channel = connection.CreateModel();
+
+channel.ExchangeDeclare(exchange: "fanout-exchange-example", type: ExchangeType.Fanout);
+
+Console.Write("Kuyruk Adını Giriniz : ");
+string _queueName = Console.ReadLine();
+
+channel.QueueDeclare(queue: _queueName, exclusive: false);
+
+channel.QueueBind(queue: _queueName, exchange: "fanout-exchange-example", routingKey: string.Empty);
+
+EventingBasicConsumer consumer = new(channel);
+
+channel.BasicConsume(queue: _queueName, autoAck: true, consumer: consumer);
+
+consumer.Received += (sender, e) =>
+{
+    string message = Encoding.UTF8.GetString(e.Body.Span);
+    Console.WriteLine(message);
+};
+
+Console.Read();
---
+++ RabbitMQ.Fanout.Exchange.Publisher/Program.cs
@@ -0,0 +1,21 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+ConnectionFactory connectionFactory = new();
+connectionFactory.Uri = new("your_url");
+
+using IConnection connection = connectionFactory.CreateConnection();
+using IModel channel = connection.CreateModel();
+
+channel.ExchangeDeclare(exchange: "fanout-exchange-example", type: ExchangeType.Fanout);
+
+for (int i = 0; i < 100; i++)
+{
+    await Task.Delay(200);
+    byte[] message = Encoding.UTF8.GetBytes("Merhaba : " + i);
+    channel.BasicPublish(exchange: "fanout-exchange-example", routingKey: string.Empty, body: message);
+}
+
+Console.Read();
+
+// Burada dikkat edilmesi gereken husus BasicPublish fonksiyonunda routingKey parametresini boş olarak veriyoruz eğer routingKey vererek belirli bir route'a mesaj göndermek istiyorsak Fanout Exchange Kullanmamalıyız!
