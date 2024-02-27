79bc795eea15fdbec63bdc927e128af229251193
İsa Talha MENGİ <italhamengi@gmail.com>
2024-02-27 14:08:12 +0300

    ESB & MassTransit Request/Response Message Design

---
+++ RabbitMQ.ESB.MassTransit.RequestResponse.Consumer/Program.cs
@@ -0,0 +1,19 @@
+﻿using MassTransit;
+using RabbitMQ.ESB.MassTransit.RequestResponse.Consumer.Consumers;
+
+string rabbitMQUri = "your_url";
+string queueName = "request-queue";
+
+IBusControl bus = Bus.Factory.CreateUsingRabbitMq(factory =>
+{
+    factory.Host(rabbitMQUri);
+
+    factory.ReceiveEndpoint(queueName:queueName,endpoint =>
+    {
+        endpoint.Consumer<RequestMessageConsumer>();
+    });
+});
+
+await bus.StartAsync();
+
+Console.Read();
---
+++ RabbitMQ.ESB.MassTransit.RequestResponse.Publisher/Program.cs
@@ -0,0 +1,28 @@
+﻿using MassTransit;
+using RabbitMQ.ESB.MassTransit.Shared.RequestResponseMessage;
+
+string rabbitMQUri = "your_url";
+string queueName = "request-queue";
+
+IBusControl bus = Bus.Factory.CreateUsingRabbitMq(factory =>
+{
+    factory.Host(rabbitMQUri);
+});
+
+await bus.StartAsync();
+
+IRequestClient<RequestMessage> requestClient =  bus.CreateRequestClient<RequestMessage>(new Uri($"{rabbitMQUri}/{queueName}"));
+
+int i = 0;
+while (true)
+{
+    await Task.Delay(200);
+    var response = await requestClient.GetResponse<ResponseMessage>(new()
+    {
+        MessageNo = i,
+        Text = $"{i}. request"
+    });
+    i++;
+    Console.WriteLine($"Message Received : {response.Message.Text}");
+}
+Console.Read();
